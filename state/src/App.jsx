17c9d63e0b69064943c1e373e1331f57436f9a3f
Blade21Shade <blade21shade@gmail.com>
2026-09-04 15:24:06 -0500

    Introduction to State lesson final part of assignment

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -5,9 +5,11 @@ const COLORS = ['pink', 'green', 'blue', 'yellow', 'purple'];
 
 function App() {
   const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [backgroundColorChangeCount, setBackgroundColorChangeCount] = useState(0);
 
   const onButtonClick = (color) => () => {
     setBackgroundColor(color);
+    setBackgroundColorChangeCount(backgroundColorChangeCount + 1);
   };
 
   return (
@@ -27,6 +29,7 @@ function App() {
           {color}
         </button>
       ))}
+      <div>{backgroundColorChangeCount}</div>
     </div>
   );
 }
